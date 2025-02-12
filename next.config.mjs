027d64479fa398af9aea733558dd149c817da41c
JuzuliMP <juzuli.coinedone@gmail.com>
2025-02-13 00:23:50 +0530

    chore: update project configuration for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,11 @@ try {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/JuzuliMP.github.io' : '',
   images: {
     unoptimized: true,
   },
+  basePath: '',
+  assetPrefix: '',
   eslint: {
     ignoreDuringBuilds: true,
   },
